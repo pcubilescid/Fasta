dcf8c4997f59bd606a99af5025ddab76a7e9c0af
Pacuci <you@example.com>
2018-09-25 12:05:34 +0200

    Longitud cabecera

---
+++ LOngitudResiduosYCabecera.py
@@ -0,0 +1,16 @@
+import sys
+def readFasta(fasta):
+    residuos = 0
+    with open(fasta, 'r') as f:
+        for line in f:
+            if line[0] == '>':
+                if residuos != 0:
+                    print('Residuo:', residuos)
+                    residuos = 0
+                print('\nCabecera:', len(line[1:-1]))
+                print (line[1:-1])
+
+            else:
+                residuos += len(line)
+
+readFasta(sys.argv[1])
