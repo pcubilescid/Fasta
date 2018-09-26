0ce3b7286c3a1b15ce06e66fa97012bed4c630da
Pacuci <you@example.com>
2018-09-26 16:48:39 +0200

    AcabadoNucleotidos

--- Nucleotidos.py
+++ Nucleotidos.py
@@ -1,24 +1,39 @@
 import sys
 import gzip
 def readFasta(fasta):
-    Nucleotidos= {'TTT': 'K', 'TTC': 'K', 'TTA': 'N', 'TTG': 'N', 'TCT': 'R', 'TCC': 'R', 'TCA': 'S', 'TCG': 'S',
-                  'TAT': 'I', 'TAC': 'M', 'TAA': 'I', 'TAG': 'I', 'TGT': 'T', 'TGC': 'T', 'TGA': 'U', 'TGG': 'T',
-                  'CTT': 'E', 'CTC': 'E', 'CTA': 'D', 'CTG': 'D', 'CCT': 'G', 'CCC': 'G', 'CCA': 'G', 'CCG': 'G',
-                  'CAT': 'V', 'CAC': 'V', 'CAA': 'V', 'CAG': 'V', 'CGT': 'A', 'CGC': 'A', 'CGA': 'A', 'CGG': 'A',
-                  'ATT': '-', 'ATC': '-', 'ATA': 'Y', 'ATG': 'Y', 'ACT': '-', 'ACC': 'W', 'ACA': 'W', 'ACG': 'Y',
-                  'AAT': 'L', 'AAC': 'L', 'AAA': 'F', 'AAG': 'F', 'AGT': 'S', 'AGC': 'S', 'AGA': 'S', 'AGG': 'S',
-                  'GTT': 'Q', 'GTC': 'Q', 'GTA': 'H', 'GTG': 'H', 'GCT': 'R', 'GCC': 'R', 'GCA': 'R', 'GCG': 'R',
-                  'GAT': 'L', 'GAC': 'L', 'GAA': 'L', 'GAG': 'L', 'GGT': 'P', 'GGC': 'P', 'GGA': 'P', 'GGG': 'P'}
-    traducir = 0;
+    Nucleotidos= {'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+    proteina= ''
 
     with gzip.open(fasta, 'r') as f:
         for line in f:
             line=line.decode()
-            if line[0] != '>':
-                print (line[0:3:3])
-                #traducir += line
-            if traducir == 3:
-                traducir = 0;
-                print(traducir)
+            line= line.replace("\n", "")
+            line = line.replace("\r", "")
+            if line[0] == '>':
+                print(line)
+                print(proteina)
+                proteina=''
+            else:
+                if len(line) % 3 == 0:
+                    for i in range(0, len(line), 3):
+                        codon = line[i: i + 3]
+                        proteina += Nucleotidos[codon]
+
 
 readFasta(sys.argv[1])
